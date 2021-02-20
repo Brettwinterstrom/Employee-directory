354c6007360f0f6abfcc99d1d8b9799b100aa9f9
Brett Winterstrom <brettwinterstrom@yahoo.com>
2021-02-19 17:02:55 -0800

    initial commit and added basic navbar

---
+++ employees/src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav className="navbar navstyle">
+            <h1>Employee Directory</h1>
+            <p>search employees below</p>
+        </nav>
+    );
+}
+
+export default Navbar;
